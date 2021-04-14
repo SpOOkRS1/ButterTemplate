a972852fbfed1808f096c8f4116dc4c4c735c903
Insensual <>
2021-04-14 01:27:43 +0000

    Everything is working

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# As of April 13th everything is working and this is the last line of code that has been written. If anything is broken after this repl sucks. I won't be editing this anymore, this will serve as a template for my next project
 from website import create_app
 
 app = create_app()
