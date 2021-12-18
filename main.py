0347241b6540534bab7bdcb7601be3eae3fd8bb0
Insensual <77132549+SpOOkRS1@users.noreply.github.com>
2021-12-18 21:47:52 +0000

    Agera initial commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# As of April 13th everything is working and this is the last line of code that has been written. If anything is broken after this repl sucks. I won't be editing this anymore, this will serve as a template for my next project
+# As of December 18th Agera 
 from website import create_app
 
 app = create_app()
