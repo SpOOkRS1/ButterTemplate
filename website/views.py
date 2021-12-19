3508e7766b3a5e2cb421327ac1482511a985fe47
Insensual <77132549+SpOOkRS1@users.noreply.github.com>
2021-12-19 02:53:50 +0000

    Home page nearly setup

--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
-from .models import Note
+from .models import Project
 from . import db
 import json
 
@@ -8,7 +8,7 @@ views = Blueprint('views', __name__)
 
 
 @views.route('/', methods=['GET', 'POST'])
-@login_required
+# @login_required
 def home():
     if request.method == 'POST':
         note = request.form.get('note')
@@ -21,16 +21,4 @@ def home():
             db.session.commit()
             flash('Note added!', category='success')
 
-    return render_template("home.html", user=current_user)
-
-
-@views.route('/delete-note', methods=['POST'])
-def delete_note():
-    note = json.loads(request.data)
-    noteId = note['noteId']
-    note = Note.query.get(noteId)
-    if note:
-        if note.user_id == current_user.id:
-            db.session.delete(note)
-            db.session.commit()
-    return jsonify({})
+    return render_template("home.html")
